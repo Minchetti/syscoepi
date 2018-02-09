37f4a433546332e427184794547b19381ae26b10
Marcello Minchetti <minchetti@fitassul.local>
2018-02-09 10:57:38 -0200

    ajax feito com sucesso para um serv de teste

--- src/atomos/login/Login.js
+++ src/atomos/login/Login.js
@@ -32,12 +32,14 @@ const Login = props => (
     <form className="mt-20 text-left">
       <div className="form-group">
         <label for="exampleInputEmail1">User</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        {/* <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Enter email"/> */}
+        <input type="text" className="form-control" id="user-login" aria-describedby="emailHelp" placeholder="Enter user"/>
         {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
       </div>
       <div className="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        {/* <input type="password" className="form-control" id="senha-login" placeholder="Password"/> */}
+        <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Enter email"/>
       </div>
       {/* <div className="form-check">
         <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
