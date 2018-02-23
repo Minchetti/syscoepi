2d18794bb212f96271a8bcfa3d8bd858c266b48c
Marcello Minchetti <minchetti@fitassul.local>
2018-02-23 14:59:42 -0300

    trabalhando na validação do form d cadastro de novo usuario, mascara de campos OK

--- src/atomos/criarconta/CriarConta.js
+++ src/atomos/criarconta/CriarConta.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+{/* <script src="https://cdnjs.com/libraries/jquery.mask"></script> */}
+
+
+
 // @flow
 const CriarConta = props => (
  
-
 <div className="p-30">
 
-<div className="modal fade md-lg" id="modal-criar-conta" tabIndex="-1" role="dialog" aria-labelledby="modal-criar-conta-label" aria-hidden="true">
-  <div className="modal-dialog modal-lg" role="document">
+<div className="modal fade" id="modal-criar-conta" tabIndex="-1" role="dialog" aria-labelledby="modal-criar-conta-label" aria-hidden="true">
+  <div className="modal-dialog " role="document">
     <div className="modal-content z-9999">
       <div className="modal-header d-flex justify-content-between align-center">
         <div className="d-flex align-center">
@@ -22,81 +25,41 @@ const CriarConta = props => (
 
 
       <div className="modal-body ">
-        <form className="text-left d-flex flex-wrap">
+        <form id="form-criar-conta" className="text-left d-flex flex-wrap" method="post" action="">
           <div className="form-group col-md-6">
-            <label for="exampleInputPassword1" className="pr-10"><i className="fa fa-user pr-2" aria-hidden="true"/>CNPJ: </label>
-            <input type="text" className="form-control" id="input-cnpj" aria-describedby="emailHelp" placeholder="Ex: 71.340.830/0001-06" value="71.340.830/0001-06"/>
+            <label for="input-nome"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Nome</label>
+            <input type="text" className="form-control" id="input-nome" aria-describedby="emailHelp" required />
           </div>
           <div className="form-group col-md-6">
-            <label for="exampleInputPassword1" className="pr-10"><i className="fa fa-user pr-2" aria-hidden="true"/>Nome Amigável: </label>
-            <input type="text" className="form-control" id="input-nome-amigavel" aria-describedby="emailHelp" placeholder="Ex: Fitassul" value="Marcello"/>
+            <label for="input-cpf"><i className="fa fa-truck pr-2" aria-hidden="true"></i>CPF</label>
+            <input type="text" className="form-control" id="input-cpf" aria-describedby="emailHelp" required/>
           </div>
           <div className="form-group col-md-6">
-            <label for="exampleInputPassword1" className="pr-10"><i className="fa fa-user pr-2" aria-hidden="true"/>Razão Social: </label>
-            <input type="text" className="form-control" id="input-razao-social" aria-describedby="emailHelp" placeholder="Ex: Fitassul Comércio LTDFA" value="Marcello LTDA"/>
+            <label for="input-celular"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Celular</label>
+            <input type="text" className="form-control" id="input-celular" />
           </div>
           <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>CEP</label>
-            <input type="text" className="form-control" id="input-cep" aria-describedby="emailHelp" placeholder="Ex: 37.501-143" value="37.501-143"/>
+            <label for="input-email"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Email</label>
+            <input type="email" className="form-control" id="input-email" aria-describedby="emailHelp" required/>
           </div>
           <div className="form-group col-md-6">
-            <label for="exampleInputPassword1" className="pr-10"><i className="fa fa-building pr-2" aria-hidden="true"/>Logradouro: </label>
-            <input type="text" className="form-control" id="input-logradouro" aria-describedby="emailHelp" placeholder="Ex: Rua Silvestre Ferraz" value="Rua Silvestre Ferraz"/>
+            <label for="input-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Senha</label>
+            <input type="password" className="form-control" id="input-senha" aria-describedby="emailHelp" required/>
           </div>
           <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Complemento</label>
-            <input type="text" className="form-control" id="input-complemento" aria-describedby="emailHelp" placeholder="Ex: Portão Azul" value="Portão Azul"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Número</label>
-            <input type="text" className="form-control" id="input-numero" aria-describedby="emailHelp" placeholder="Ex: 422" value="455"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Bairro</label>
-            <input type="text" className="form-control" id="input-bairro" aria-describedby="emailHelp" placeholder="Ex: Centro" value="Centro"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Municipio</label>
-            <input type="text" className="form-control" id="input-municipio" aria-describedby="emailHelp" placeholder="Ex: Itajubá" value="Brazópolis"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>UF</label>
-            <input type="text" className="form-control" id="input-uf" aria-describedby="emailHelp" placeholder="Ex: MG" value="MG"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>IBGE</label>
-            <input type="text" className="form-control" id="input-ibge" aria-describedby="emailHelp" placeholder="EX: 31212121" value="1357924"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Tipo de Endereço</label>
-            <input type="text" className="form-control" id="input-tipo-endereco" aria-describedby="emailHelp" placeholder="Ex: 0" value="1"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Nome</label>
-            <input type="text" className="form-control" id="input-nome" aria-describedby="emailHelp" placeholder="Ex: Luciano" value="Marcello"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Email</label>
-            <input type="email" className="form-control" id="input-email" aria-describedby="emailHelp" placeholder="Ex: luciano@fitassul.com.br" value="minchettimarcello@gmail.com"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Senha</label>
-            <input type="text" className="form-control" id="input-senha" aria-describedby="emailHelp" placeholder="Ex: 123456" value="123321"/>
-          </div>
-          <div className="form-group col-md-6">
-            <label for="exampleFormControlSelect1"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Confirma Senha</label>
-            <input type="text" className="form-control" id="input-confirma-senha" aria-describedby="emailHelp" placeholder="Ex: 123456" value="123321"/>
+            <label for="input-confirma-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Confirma Senha</label>
+            <input type="password" className="form-control" id="input-confirma-senha" aria-describedby="emailHelp" required/>
           </div>
           {/* <div className="form-group col-md-6">
             <label for="exampleFormControlSelect1">
-              <i className="fa fa-clock-o pr-2" aria-hidden="true"></i>Turno
+            <i className="fa fa-clock-o pr-2" aria-hidden="true"></i>Turno
             </label>
             <select className="form-control" id="exampleFormControlSelect1">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
             </select>
           </div> */}
         </form>        
