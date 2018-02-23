050bad41fa73a58afe02e73192b16ce4ee53159f
Marcello Minchetti <minchetti@fitassul.local>
2018-02-23 17:24:55 -0300

    trocado o validate do jquery para o valitador do bootstrap, funciona melhor, trabalhar agr na validação de cpf

--- src/atomos/criarconta/CriarConta.js
+++ src/atomos/criarconta/CriarConta.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-{/* <script src="https://cdnjs.com/libraries/jquery.mask"></script> */}
 
 
 
@@ -25,30 +24,36 @@ const CriarConta = props => (
 
 
       <div className="modal-body ">
-        <form id="form-criar-conta" className="text-left d-flex flex-wrap" method="post" action="">
+        <form id="form-criar-conta" className="text-left d-flex flex-wrap" method="post" action="" data-toggle="validator" role="form">
           <div className="form-group col-md-6">
             <label for="input-nome"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Nome</label>
             <input type="text" className="form-control" id="input-nome" aria-describedby="emailHelp" required />
+            <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
             <label for="input-cpf"><i className="fa fa-truck pr-2" aria-hidden="true"></i>CPF</label>
-            <input type="text" className="form-control" id="input-cpf" aria-describedby="emailHelp" required/>
+            <input type="text" className="form-control" id="input-cpf" name="input-cpf" aria-describedby="emailHelp" required/>
+            <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
             <label for="input-celular"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Celular</label>
             <input type="text" className="form-control" id="input-celular" />
+            <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
             <label for="input-email"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Email</label>
             <input type="email" className="form-control" id="input-email" aria-describedby="emailHelp" required/>
+            <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
             <label for="input-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Senha</label>
             <input type="password" className="form-control" id="input-senha" aria-describedby="emailHelp" required/>
+            <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
             <label for="input-confirma-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Confirma Senha</label>
-            <input type="password" className="form-control" id="input-confirma-senha" aria-describedby="emailHelp" required/>
+            <input type="password" className="form-control" id="input-confirma-senha" aria-describedby="emailHelp" data-match="#input-senha" data-match-error="Atenção! As senhas não estão iguais." required/>
+            <div class="help-block with-errors"></div>
           </div>
           {/* <div className="form-group col-md-6">
             <label for="exampleFormControlSelect1">
