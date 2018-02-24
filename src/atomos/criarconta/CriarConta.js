6e45873f255aa4182d1214d1c63a2d43baad9d75
Marcello Minchetti <minchetti@fitassul.local>
2018-02-24 14:13:29 -0300

    uhAUhsh

--- src/atomos/criarconta/CriarConta.js
+++ src/atomos/criarconta/CriarConta.js
@@ -26,32 +26,32 @@ const CriarConta = props => (
       <div className="modal-body ">
         <form id="form-criar-conta" className="text-left d-flex flex-wrap" method="post" action="" data-toggle="validator" role="form">
           <div className="form-group col-md-6">
-            <label for="input-nome"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Nome</label>
+            <label for="input-nome"><i className="fa fa-user pr-2" aria-hidden="true"></i>Nome</label>
             <input type="text" className="form-control" id="input-nome" aria-describedby="emailHelp" required />
             <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
-            <label for="input-cpf"><i className="fa fa-truck pr-2" aria-hidden="true"></i>CPF</label>
+            <label for="input-cpf"><i className="fa fa-id-card-o pr-2" aria-hidden="true"></i>CPF</label>
             <input type="text" className="form-control" id="input-cpf" name="input-cpf" aria-describedby="emailHelp" required/>
             <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
-            <label for="input-celular"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Celular</label>
+            <label for="input-celular"><i className="fa fa-address-book pr-2" aria-hidden="true"></i>Celular</label>
             <input type="text" className="form-control" id="input-celular" />
             <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
-            <label for="input-email"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Email</label>
+            <label for="input-email"><i className="fa fa-envelope pr-2" aria-hidden="true"></i>Email</label>
             <input type="email" className="form-control" id="input-email" aria-describedby="emailHelp" required/>
             <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
-            <label for="input-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Senha</label>
+            <label for="input-senha"><i className="fa fa-key pr-2" aria-hidden="true"></i>Senha</label>
             <input type="password" className="form-control" id="input-senha" aria-describedby="emailHelp" required/>
             <div class="help-block with-errors"></div>
           </div>
           <div className="form-group col-md-6">
-            <label for="input-confirma-senha"><i className="fa fa-truck pr-2" aria-hidden="true"></i>Confirma Senha</label>
+            <label for="input-confirma-senha"><i className="fa fa-key pr-2" aria-hidden="true"></i>Confirma Senha</label>
             <input type="password" className="form-control" id="input-confirma-senha" aria-describedby="emailHelp" data-match="#input-senha" data-match-error="Atenção! As senhas não estão iguais." required/>
             <div class="help-block with-errors"></div>
           </div>
