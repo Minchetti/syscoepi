a229e643dd80dc74b50c3e01cb00c1a4b1b55035
Marcello Minchetti <minchetti@fitassul.local>
2018-02-26 17:37:30 -0300

    ajuste no front d empresas

--- src/atomos/company/Company.js
+++ src/atomos/company/Company.js
@@ -17,7 +17,7 @@ const Company = props => (
 
 
   <div id="company" className="">
-    <h4 className="mt-10"><i className="fa fa-building" aria-hidden="true"></i> Empresas</h4>        
+    <h4 className="mt-10 mb-20"><i className="fa fa-building" aria-hidden="true"></i> Empresas</h4>        
 
         <div className=""> 
           {/* <Button class="btn-dark" icon="fa-user-plus" text="Adicionar"/> */}
@@ -26,29 +26,29 @@ const Company = props => (
           <div className=" d-flex flex-column">
             
             
-            <div className="d-flex align-center p-0 select-company">
-              <div className="col-md-11 p-0">
-                <select class="form-control">
-                  <option>Metrosul</option>
-                  <option>BrazOffice</option>
-                  <option>Fitassul</option>
-                  <option>Helibras</option>
-                </select>
+            <div className="d-flex mb-20">
+              <div className="col-md-4 d-flex align-center p-0">
+                <div className="col-md-11 p-0">
+                  <select class="form-control">
+                    <option>Metrosul</option>
+                    <option>BrazOffice</option>
+                    <option>Fitassul</option>
+                    <option>Helibras</option>
+                  </select>
+                </div>
+                <div className="col-md-1 p-0">
+                  <Button class="btn-dark m-0 col" icon="fa-plus fa-lg"/>
+                </div>
               </div>
-              <div className="col-md-1 p-0">
-                <Button class="btn-dark m-0 col" icon="fa-plus fa-lg"/>
+              <div className="col-md-8 p-0 align-center">
+                <Button class="btn-dark" icon="fa-info pr-10" text="Dados" target="#criar-conta"/>
+                <Button class="btn-dark ml-10" icon="fa-user-plus pr-10" text="Importar Funcionários"/> 
+                <Button class="btn-dark ml-10" icon="fa-plus pr-10" text="Criar GH"/> 
+                <Button class="btn-dark ml-10" icon="fa-plus pr-10" text="Criar CC"/>
               </div>
             </div>
 
 
-
-            <h3 className="mt-30">Metrosul</h3>
-            <div className="col-md-12 d-flex align-center justify-content-center mb-20">
-              <Button class="btn-dark" icon="fa-info pr-10" text="Dados da Empresa" target="#criar-conta"/>
-              <Button class="btn-dark ml-10" icon="fa-user-plus pr-10" text="Importar Funcionários"/> 
-              <Button class="btn-dark ml-10" icon="fa-plus pr-10" text="Criar GH"/> 
-              <Button class="btn-dark ml-10" icon="fa-plus pr-10" text="Criar CC"/> 
-            </div>
 
             <Table1 />
             <Table2 />
