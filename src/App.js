ceeebc477f7185d52240c94735a2eec7752637d9
Marcello Minchetti <minchetti@fitassul.local>
2018-03-09 16:03:11 -0300

    req com fetch pegando os campos do form pelo state com sucesso, agr eh deixar as rotas seguras funcionadno

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './atomos/home';
+import Login from './atomos/login';
 import API from 'fetch-api';
 // import $ from 'https://code.jquery.com/jquery-2.2.4.js';
 
@@ -19,9 +20,10 @@ class App extends Component {
     return (
       <div className="App">     
 
+      
 
 
-        <Router>
+         <Router>
           <div>
             <AuthButton />
             <ul>
@@ -39,8 +41,8 @@ class App extends Component {
         </Router>
 
 
-        {/* <Login />         */}
-        <Home />   
+      {/* <Home />     */}
+      {/* <Login />         */}
         
         <div className="modal-loading">
           <i id="loading" className="fa fa-spinner" aria-hidden="true" ></i>
@@ -49,31 +51,31 @@ class App extends Component {
 
 
 
-<div id="Login" className="p-absolute col h-100 ">
-        <div className="boxLogin pt-80 p-absolute">
-        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
-        <div className="t-0 l-0 r-0 p-absolute d-nonin" id="msg-validou">
-        <i className="fa fa-check" aria-hidden="true"></i> Email validado com sucesso. Faça seu login.
-        </div>
-        <form className="mt-20 text-left">
-        <div className="form-group">
+{/* <div id="Login" className="p-absolute col h-100 ">
+  <div className="boxLogin pt-80 p-absolute">
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
+    <div className="t-0 l-0 r-0 p-absolute d-nonin" id="msg-validou">
+      <i className="fa fa-check" aria-hidden="true"></i> Email validado com sucesso. Faça seu login.
+    </div>
+    <form onSubmit={ this.onSubmit } className="mt-20 text-left">
+      <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email</label>
-        <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
-        </div>
-        <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Senha</label>
-                <input type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
-                </div>
-                <button onClick={this.login} id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
-                <i className="fa fa-sign-in pr-2" aria-hidden="true"/>Entrar
-                </button>
-                <div className="d-flex justify-content-between w-100">
-                <span id="lnk-criar-conta" className="c-pointer" data-toggle="modal" data-target="#modal-usuario">Criar Conta</span> 
-                <span className="c-pointer">Esqueci a Senha</span>     
-                </div>
-                </form>
-                </div>
-              </div> 
+        <input value={state.email} onChange={ e => this.setState({ email : e.target.value }) } type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Senha</label>
+        <input value={state.senha} onChange={ e => this.setState({ senha : e.target.value }) } type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
+      </div>
+      <button onClick={this.login} id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
+        <i className="fa fa-sign-in pr-2" aria-hidden="true"/>Entrar
+      </button>
+      <div className="d-flex justify-content-between w-100">
+        <span id="lnk-criar-conta" className="c-pointer" data-toggle="modal" data-target="#modal-usuario">Criar Conta</span> 
+        <span className="c-pointer">Esqueci a Senha</span>     
+      </div>
+    </form>
+  </div>
+</div>  */}
 
 
 
@@ -145,88 +147,63 @@ const Protected = () => <h3>Protected</h3>;
 
 
 
-class Login extends React.Component {
-  state = {
-    redirectToReferrer: false
-  };  
+// class Login extends React.Component {
+//   state = {
+//     redirectToReferrer: false
+//   };  
   
-  login = () => {
-    const dados = {
-      Email: 'minchettimarcello@gmail.com',
-      Senha: '123'
-    }
-    fetch("http://192.168.10.30/v1/clientes/login",{ 
-      method: 'post',
-      body: JSON.stringify(dados),
-      headers: {
-        'content-type': 'application/json'
-      },
-    })
-    .then(function(response){
-        response.json().then(function(data){
-          console.log(data);
+//   login = () => {
+//     const dados = {
+//       Email: 'minchettimarcello@gmail.com',
+//       Senha: '123'
+//     }
+//     fetch("http://192.168.10.30/v1/clientes/login",{ 
+//       method: 'post',
+//       body: JSON.stringify(dados),
+//       headers: {
+//         'content-type': 'application/json'
+//       },
+//     })
+//     .then(function(response){
+//         response.json().then(function(data){
+//           console.log(data);
 
-          if (data.success == true){
-          }
-        });
-    })
-    .catch(function(err){
-      console.error('Failed retrieving information', err);
-      alert('nao deu');
-    });
+//           if (data.success == true){
+//           }
+//         });
+//     })
+//     .catch(function(err){
+//       console.error('Failed retrieving information', err);
+//       alert('nao deu');
+//     });
     
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true });
-    });
+//     fakeAuth.authenticate(() => {
+//       this.setState({ redirectToReferrer: true });
+//     });
     
-  };
+//   };
   
   
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" } };
-    const { redirectToReferrer } = this.state;
+//   render() {
+//     const { from } = this.props.location.state || { from: { pathname: "/" } };
+//     const { redirectToReferrer } = this.state;
 
-    if (redirectToReferrer) {
-      return <Redirect to={from} />;
-    }
-    return (
-      <div>
-        <p>You must log in to view the page at {from.pathname}</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    );
-  }
-}
+//     if (redirectToReferrer) {
+//       return <Redirect to={from} />;
+//     }
+//     return (
+//       <div>
+//         <p>You must log in to view the page at {from.pathname}</p>
+//         <button onClick={this.login}>Log in</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
 
 
-        {/* <div id="Login" className="p-absolute col h-100 ">
-        <div className="boxLogin pt-80 p-absolute">
-        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
-        <div className="t-0 l-0 r-0 p-absolute d-nonin" id="msg-validou">
-        <i className="fa fa-check" aria-hidden="true"></i> Email validado com sucesso. Faça seu login.
-        </div>
-        <form className="mt-20 text-left">
-        <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Email</label>
-        <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
-        </div>
-        <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Senha</label>
-                <input type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
-                </div>
-                <button onClick={this.login} id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
-                <i className="fa fa-sign-in pr-2" aria-hidden="true"/>Entrar
-                </button>
-                <div className="d-flex justify-content-between w-100">
-                <span id="lnk-criar-conta" className="c-pointer" data-toggle="modal" data-target="#modal-usuario">Criar Conta</span> 
-                <span className="c-pointer">Esqueci a Senha</span>     
-                </div>
-                </form>
-                </div>
-              </div> */}
-                                          
+       
               
               
       // const dados = {      
--- src/atomos/login/Login.js
+++ src/atomos/login/Login.js
@@ -3,66 +3,157 @@ import React from 'react';
 import ModalUsuario from '../modalusuario';
 import './login.css';
 
+import API from 'fetch-api';
+
 import logo from '../../logo.svg';
 
 
-// @flow
+// const Login = props => (
+
+// <div id="Login" className="p-absolute col h-100 ">
+//   <div className="boxLogin pt-80 p-absolute">
+//     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
+//     <div className="t-0 l-0 r-0 p-absolute d-nonin" id="msg-validou">
+//       <i className="fa fa-check" aria-hidden="true"></i> Email validado com sucesso. Faça seu login.
+//     </div>
+    
+//     <h1 className="h1L">
+//       <em>S</em>
+//       <em>y</em>
+//       <em>S</em>
+//       <em>C</em>
+//       <em className="planet left"><img src={logo} className="App-logoL" alt="logo" /></em>
+//       <em>E</em>
+//       <em>P</em>
+//       <em>I</em>
+//     </h1>
 
 
-const Login = props => (
+//     <form className="mt-20 text-left">
+//       <div className="form-group">
+//         <label htmlFor="exampleInputEmail1">Email</label>
+//         <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
+//       </div>
+//       <div className="form-group">
+//         <label htmlFor="exampleInputPassword1">Senha</label>
+//         <input type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
+//       </div>
+//       <button id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
+//         <i className="fa fa-sign-in pr-2" aria-hidden="true"/>Entrar
+//       </button>
+//       <div className="d-flex justify-content-between w-100">
+//         <span id="lnk-criar-conta" className="c-pointer" data-toggle="modal" data-target="#modal-usuario">Criar Conta</span> 
+//         <span className="c-pointer">Esqueci a Senha</span>     
+//       </div>
 
-<div id="Login" className="p-absolute col h-100 ">
+//     </form>
+
+//     </div>
+
+
+
+
+//     <ModalUsuario name="Criar Usuário"/>
+// </div>
+
+
+
+// );
+
+
+// Login.propTypes = {
+//   text: PropTypes.string.isRequired,
+// };
+
+
+
+
+
+
+
+
+class Login extends React.Component {
+      state = {
+        email : '',
+        senha : ''
+      }
+
+
+  onSubmit = e => {         
+    fetch("http://192.168.10.30/v1/clientes/login",{ 
+      method: 'post',
+      body: JSON.stringify(this.state),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+    .then(function(response){
+        response.json().then(function(data){
+          console.log(data);
+
+          if (data.success == true){
+            alert(data.message);
+          }
+          else{
+            alert(data.message);
+
+          }
+          
+        });
+    })
+    .catch(function(err){
+      console.error('Failed retrieving information', err);
+      alert('nao deu');
+    });
+
+  }
+
+
+  render(){
+    
+    return (
+
+<div>
   <div className="boxLogin pt-80 p-absolute">
     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
     <div className="t-0 l-0 r-0 p-absolute d-nonin" id="msg-validou">
       <i className="fa fa-check" aria-hidden="true"></i> Email validado com sucesso. Faça seu login.
     </div>
-    
-    <h1 className="h1L">
-      <em>S</em>
-      <em>y</em>
-      <em>S</em>
-      <em>C</em>
-      <em className="planet left"><img src={logo} className="App-logoL" alt="logo" /></em>
-      <em>E</em>
-      <em>P</em>
-      <em>I</em>
-    </h1>
-
-
-    <form className="mt-20 text-left">
+    <form onSubmit={ this.onSubmit } className="mt-20 text-left">
       <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email</label>
-        <input type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
+        <input value={this.state.email} onChange={ e => this.setState({ email : e.target.value }) } type="email" className="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Digite o email"/>
       </div>
       <div className="form-group">
         <label htmlFor="exampleInputPassword1">Senha</label>
-        <input type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
+        <input value={this.state.senha} onChange={ e => this.setState({ senha : e.target.value }) } type="password" className="form-control" id="senha-login" aria-describedby="emailHelp" placeholder="Digite a senha"/>
       </div>
-      <button id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
+      <button onClick={this.login} id="logar" type="submit" className="btn btn-primary btn-block btn-large mb-2">
         <i className="fa fa-sign-in pr-2" aria-hidden="true"/>Entrar
       </button>
       <div className="d-flex justify-content-between w-100">
         <span id="lnk-criar-conta" className="c-pointer" data-toggle="modal" data-target="#modal-usuario">Criar Conta</span> 
         <span className="c-pointer">Esqueci a Senha</span>     
       </div>
-
     </form>
-
-    </div>
-
-
-
-
-    <ModalUsuario name="Criar Usuário"/>
-</div>
-
+  </div>
+</div> 
 
 
 );
+}
+}
 
-// Login.propTypes = {
-//   text: PropTypes.string.isRequired,
-// };
+
+
+
 
 export default Login;
+
+
+
+
+
+
+
+
