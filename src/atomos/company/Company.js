9d9a87438014a51ffa2ee0c4358e831300d35eb5
Marcello Minchetti <minchetti@fitassul.local>
2018-04-23 19:02:44 -0300

    working company

--- src/atomos/company/Company.js
+++ src/atomos/company/Company.js
@@ -61,7 +61,7 @@ class Company extends React.Component {
     if(document.getElementById("company-cnpj").value.replace(/[./-]/g, "") !== this.state.dadosInicias.cnpj || 
        document.getElementById("company-nome-amigavel").value !== this.state.dadosInicias.nomeAmigavel ||
        document.getElementById("company-razao-social").value !== this.state.dadosInicias.razaoSocial){
-          document.getElementById("editarEmpresa").parentNode.style.display = "block";
+          document.getElementById("editarEmpresa").parentNode.style.display = "flex";
     }
     else{
       document.getElementById("editarEmpresa").parentNode.style.display = "none";
@@ -100,9 +100,9 @@ class Company extends React.Component {
 
     //TESTE
     var data = [
-      {cnpj:'123999123', nomeAmigavel:'Empresa1', razaoSocial: 'Razao1'},    
-      {cnpj:'123678123', nomeAmigavel:'Empresa2', razaoSocial: 'Razao2'},
-      {cnpj:'123123123', nomeAmigavel:'Empresa3', razaoSocial: 'Razao3'}
+      {cnpj:'123999123', nomeAmigavel:'Empresa 1', razaoSocial: 'Razao1'},    
+      {cnpj:'123678123', nomeAmigavel:'Empresa 2', razaoSocial: 'Razao2'},
+      {cnpj:'123123123', nomeAmigavel:'Empresa 3', razaoSocial: 'Razao3'}
     ]
     this.setState({ arrayEmpresas: data });  
     this.setState({ empresaSelecionada: data[0].nomeAmigavel });  
@@ -138,27 +138,27 @@ class Company extends React.Component {
     // });
 
     var data = [
-      {cnpj: '12345678998765', nomeAmigavel:'Empresa1', razaoSocial: 'Razao1'} 
+      {cnpj: '12345678998765', nomeAmigavel:'Empresa 1', razaoSocial: 'Razao1'} 
     ]
     var data2 = [
-      {cnpj: '222222', nomeAmigavel:'Empresa2', razaoSocial: 'Razao2'} 
+      {cnpj: '222222', nomeAmigavel:'Empresa 2', razaoSocial: 'Razao2'} 
     ]
     var data3 = [
-      {cnpj: '333333', nomeAmigavel:'Empresa3', razaoSocial: 'Razao3 '} 
+      {cnpj: '333333', nomeAmigavel:'Empresa 3', razaoSocial: 'Razao3 '} 
     ]
-    if (this.state.empresaSelecionada == 'Empresa1' ){
+    if (this.state.empresaSelecionada == 'Empresa 1' ){
       this.setState({ nomeAmigavel: data[0].nomeAmigavel });  
       this.setState({ razaoSocial: data[0].razaoSocial });  
       this.setState({ cnpj: data[0].cnpj });   
       this.setState({ dadosInicias: data[0]});   
     }    
-    if (this.state.empresaSelecionada == 'Empresa2' ){
+    if (this.state.empresaSelecionada == 'Empresa 2' ){
       this.setState({ nomeAmigavel: data2[0].nomeAmigavel });  
       this.setState({ razaoSocial: data2[0].razaoSocial });  
       this.setState({ cnpj: data2[0].cnpj });   
       this.setState({ dadosInicias: data2[0]}); 
     }
-    if (this.state.empresaSelecionada == 'Empresa3' ){
+    if (this.state.empresaSelecionada == 'Empresa 3' ){
       this.setState({ nomeAmigavel: data3[0].nomeAmigavel });  
       this.setState({ razaoSocial: data3[0].razaoSocial });  
       this.setState({ cnpj: data3[0].cnpj });   
@@ -209,23 +209,23 @@ class Company extends React.Component {
           <div className="col-md-12 mt-5">  
 
             <form id="form-usuario" onSubmit={this.onSubmit} onChange={this.onChange} className="text-left d-flex flex-wrap" method="post" action="">
-              <div class="form-row"> 
+              <div class="form-row w-100"> 
 
-                <div className="form-group col-md-3">
+                <div className="form-group col-md-3 mb-0 d-flex align-items-center flex-wrap col-md-3">
                   <label htmlFor="company-cnpj"><i className="far fa-id-card pr-2" ></i>CNPJ</label>
                   <InputMask mask="99.999.999/9999-99" type="text" value={this.state.cnpj} onChange={e => this.setState({ cnpj: e.target.value })} className="form-control" id="company-cnpj" name="company-cnpj" aria-describedby="cnpjHelp" required />
                 </div>               
                
-                <div className="form-group col-md-3">
+                <div className="form-group col-md-3 mb-0 d-flex align-items-center flex-wrap col-md-3">
                   <label htmlFor="company-nome-amigavel"><i className="far fa-user pr-2" ></i>Nome Amigável</label>
                   <input type="text" value={this.state.nomeAmigavel} onChange={e => this.setState({ nomeAmigavel: e.target.value })} className="form-control" id="company-nome-amigavel" name="company-nome-amigavel" aria-describedby="nomeAmigavelHelp" required />
                 </div>
                
-                <div className="form-group col-md-3">
+                <div className="form-group col-md-3 mb-0 d-flex align-items-center flex-wrap col-md-3">
                   <label htmlFor="company-razao-social"><i className="far fa-user pr-2" ></i>Razão Social</label>
                   <input type="text" value={this.state.razaoSocial} onChange={e => this.setState({ razaoSocial: e.target.value })} className="form-control" id="company-razao-social" name="company-razao-social" aria-describedby="razaoSocialHelp" required />
                 </div>
-                <div className="form-group mb-0 d-nonin">
+                <div className="form-group mb-0 col-md-3 d-nonin align-items-center justify-content-center">
                   <button type="button" id="cancelarEditar" className="btn btn-danger mr-2" data-dismiss="modal">
                     <i className="fa fa-times fa-lg pr-2"  />Fechar
                   </button>
