c286d89564d724d4e172c1faa2e7e73d0ba1b58c
Marcello Minchetti <minchetti@fitassul.local>
2019-06-11 18:08:56 -0300

    criei componente dinamico table...funcionou...ainda n criei o dinamico row...estou usando rowSimple e rowComplex

--- src/atomos/modalcriarrequisicao/ModalCriarRequisicao.js
+++ src/atomos/modalcriarrequisicao/ModalCriarRequisicao.js
@@ -72,7 +72,7 @@ class AdicionarPorFuncionario extends React.Component {
     episSelecionados: []
   }
     
-  selecionouFuncionario(func) {
+  selecionou(func) {
     this.setState({ funcionarioSelecionado: func });  
   } 
   selecionouEpi(epi) {
@@ -92,7 +92,7 @@ class AdicionarPorFuncionario extends React.Component {
     return(
       <div>
         <div className="mt-4">
-          <SearchBarFuncionarios selecionouFuncionario={this.selecionouFuncionario.bind(this)}/>
+          <SearchBar busca={"funcionarios"} selecionou={this.selecionou.bind(this)}/>
           {this.state.funcionarioSelecionado !== ""&& (
             <div>
               <div className="row mt-3">
@@ -128,7 +128,7 @@ class AdicionarPorFuncionario extends React.Component {
 
               {/* <SelecionarEpi/> */}
               
-              <SearchBarEpis selecionouEpi={this.selecionouEpi.bind(this)}/>
+              <SearchBar busca={"epis"} selecionouEpi={this.selecionouEpi.bind(this)}/>
               {this.state.episSelecionados !== "" && (
                 
                 this.state.episSelecionados.map(epiSelecionado => 
@@ -315,28 +315,40 @@ class Buttons extends React.Component {
 
 
 
-class SearchBarFuncionarios extends React.Component {
+class SearchBar extends React.Component {
   state = {
-    listaFuncionarios : "",
+    lista : "",
     searchText: ""
-
   } 
-  handleChange(event) {    
-    var data = [
-      {nome: "Marcello Minchetti", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
-      turno: "vespertino", sexo: "Masculino"},
-      {nome: "Rodolfo  Augusto", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
-      turno: "vespertino", sexo: "Masculino"},
-      {nome: "Susi  Ribeiro", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
-      turno: "vespertino", sexo: "Masculino"}       
-    ];
+  handleChange(event) {   
+    if (this.props.busca === "funcionarios"){
+      var data = [
+        {nome: "Marcello Minchetti", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
+        turno: "vespertino", sexo: "Masculino"},
+        {nome: "Rodolfo  Augusto", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
+        turno: "vespertino", sexo: "Masculino"},
+        {nome: "Susi  Ribeiro", rg: "16778405", cpf: "78970546617", email: "teste@gmail.com", gh: "123456", cc: "123456", 
+        turno: "vespertino", sexo: "Masculino"}       
+      ]
+    }
+    else if(this.props.busca === "epis"){
+      var data = [
+        {cod: "13524", nome: "Cotoveleira BreakGlass1", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+        estoque: "20"},
+        {cod: "13524", nome: "Cotoveleira BreakGlass2", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+        estoque: "20"},
+        {cod: "13524", nome: "Cotoveleira BreakGlass3", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+        estoque: "20"}        
+      ]
+    }
+
     this.setState({ searchText: event.target.value });
-    this.setState({ listaFuncionarios: data });
-    this.setState({ selecionouFuncionarioBool: false });
+    this.setState({ lista: data });
+    this.setState({ selecionouBool: false });
   }
 
-  selecionouFuncionarioBool(bool) {
-    this.setState({ selecionouFuncionarioBool: bool });   //se o usuario ja selecionou um CA na pesquisa some com a tabela de resultados da pesquisa
+  selecionouBool(bool) {
+    this.setState({ selecionouBool: bool });   //se o usuario ja selecionou um CA na pesquisa some com a tabela de resultados da pesquisa
     this.setState({ searchText: "" });  //limpar o campo de pesquisa qndo o usuario clicar em algum CA pesquisado
   }
   
@@ -344,13 +356,13 @@ class SearchBarFuncionarios extends React.Component {
     return (
       <div>
         <div className="row align-items-center">
-          <span className="col-md-5"><b>Pesquisa Funcionário</b></span>
+          <span className="col-md-5"><b>Pesquisa</b></span>
           <div className="col-md-7">
             <input style={{ zIndex: "10", position: "relative" }} className="form-control btn-sm" type="text" value={this.state.searchText} placeholder="Search..." onChange={this.handleChange.bind(this)}/>
           </div>
         </div>
-        {(this.state.listaFuncionarios !== "" && this.state.selecionouFuncionarioBool === false && this.state.searchText !== "") 
-        && <Table linhas={this.state.listaFuncionarios} selecionouFuncionario={this.props.selecionouFuncionario} selecionouFuncionarioBool={this.selecionouFuncionarioBool.bind(this)}/>} 
+        {(this.state.lista !== "" && this.state.selecionouBool === false && this.state.searchText !== "") 
+        && <Table row={this.props.busca} linhas={this.state.lista} selecionou={this.props.selecionou} selecionouBool={this.selecionouBool.bind(this)}/>} 
       </div>
     );
   }
@@ -358,47 +370,61 @@ class SearchBarFuncionarios extends React.Component {
 
 
 
-class SearchBarEpis extends React.Component {
-  state = {
-    listaEpis : "",
-    searchText: ""
-  } 
-  handleChange(event) {    
-    var data = [
-      {cod: "13524", nome: "Cotoveleira BreakGlass1", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
-      estoque: "20"},
-      {cod: "13524", nome: "Cotoveleira BreakGlass2", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
-      estoque: "20"},
-      {cod: "13524", nome: "Cotoveleira BreakGlass3", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
-      estoque: "20"}        
-    ];
 
-    this.setState({ searchText: event.target.value });
-    this.setState({ listaEpis: data });
-    this.setState({ selecionouEpiBool: false });
-  }
 
-  selecionouEpiBool(bool) {
-    this.setState({ selecionouEpiBool: bool });   //se o usuario ja selecionou um CA na pesquisa some com a tabela de resultados da pesquisa
-    this.setState({ searchText: "" });  //limpar o campo de pesquisa qndo o usuario clicar em algum CA pesquisado
-  }
+
+
+
+
+// class SearchBarEpis extends React.Component {
+//   state = {
+//     listaEpis : "",
+//     searchText: ""
+//   } 
+//   handleChange(event) {    
+//     var data = [
+//       {cod: "13524", nome: "Cotoveleira BreakGlass1", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+//       estoque: "20"},
+//       {cod: "13524", nome: "Cotoveleira BreakGlass2", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+//       estoque: "20"},
+//       {cod: "13524", nome: "Cotoveleira BreakGlass3", descricao: "Cotoveleira para Obras", validade: "10/10/2025", ca: "0776584", cc: "123456", 
+//       estoque: "20"}        
+//     ];
+
+//     this.setState({ searchText: event.target.value });
+//     this.setState({ listaEpis: data });
+//     this.setState({ selecionouEpiBool: false });
+//   }
+
+//   selecionouEpiBool(bool) {
+//     this.setState({ selecionouEpiBool: bool });   //se o usuario ja selecionou um CA na pesquisa some com a tabela de resultados da pesquisa
+//     this.setState({ searchText: "" });  //limpar o campo de pesquisa qndo o usuario clicar em algum CA pesquisado
+//   }
   
 
-  render() {
-    return (
-      <div>
-        <div className="mt-4 row align-items-center">
-          <span className="col-md-5"><b>Pesquisa Epis</b></span>
-          <div className="col-md-7">
-            <input style={{ zIndex: "10", position: "relative" }} className="form-control btn-sm" type="text" value={this.state.searchText} placeholder="Search..." onChange={this.handleChange.bind(this)}/>
-          </div>
-        </div>
-        {(this.state.listaEpis !== "" && this.state.selecionouEpiBool === false && this.state.searchText !== "") 
-        && <Table2 linhas={this.state.listaEpis} selecionouEpi={this.props.selecionouEpi} selecionouEpiBool={this.selecionouEpiBool.bind(this)}/>} 
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <div className="mt-4 row align-items-center">
+//           <span className="col-md-5"><b>Pesquisa Epis</b></span>
+//           <div className="col-md-7">
+//             <input style={{ zIndex: "10", position: "relative" }} className="form-control btn-sm" type="text" value={this.state.searchText} placeholder="Search..." onChange={this.handleChange.bind(this)}/>
+//           </div>
+//         </div>
+//         {(this.state.listaEpis !== "" && this.state.selecionouEpiBool === false && this.state.searchText !== "") 
+//         && <Table row={"epis"} linhas={this.state.listaEpis} selecionouEpi={this.props.selecionouEpi} selecionouEpiBool={this.selecionouEpiBool.bind(this)}/>} 
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
 
 
 class Table extends React.Component {
@@ -418,7 +444,8 @@ class Table extends React.Component {
         }}/>   
         <table style={{ zIndex: "10", position: "absolute", boxShadow: "0 6px 12px rgba(0,0,0,.175)", left: "222px" }}>       
           <tbody>
-            {this.props.linhas.map(linha => <Row linha={linha} selecionouFuncionario={this.props.selecionouFuncionario} selecionouFuncionarioBool={this.props.selecionouFuncionarioBool} />)}
+            {this.props.row === "funcionarios" && this.props.linhas.map(linha => <SimpleRow linha={linha} selecionou={this.props.selecionou} selecionouBool={this.props.selecionouBool} />)}
+            {this.props.row === "epis" && this.props.linhas.map(linha => <ComplexRow linha={linha} selecionouEpi={this.props.selecionouEpi} selecionouEpiBool={this.props.selecionouEpiBool} />)}
           </tbody>
         </table>
       </div>
@@ -427,54 +454,77 @@ class Table extends React.Component {
 }
 
 
-class Table2 extends React.Component {
 
-  render() {
-    return (
-      <div> 
-        <div style={{
-          opacity: "0.7",
-          position: "absolute",
-          top: "0px",
-          right: "0px",
-          bottom: "0px",
-          left: "0px",
-          zIndex: "5",
-          backgroundColor: "rgb(255, 255, 255)"
-        }}/>   
-        <table style={{ zIndex: "10", position: "absolute", boxShadow: "0 6px 12px rgba(0,0,0,.175)", left: "222px" }}>       
-          <tbody>
-            {this.props.linhas.map(linha => 
-              <Row2 linha={linha} selecionouEpi={this.props.selecionouEpi} selecionouEpiBool={this.props.selecionouEpiBool} />)}
-          </tbody>
-        </table>
-      </div>
-    );
+
+
+
+
+
+
+
+// class Table2 extends React.Component {
+
+//   render() {
+//     return (
+//       <div> 
+//         <div style={{
+//           opacity: "0.7",
+//           position: "absolute",
+//           top: "0px",
+//           right: "0px",
+//           bottom: "0px",
+//           left: "0px",
+//           zIndex: "5",
+//           backgroundColor: "rgb(255, 255, 255)"
+//         }}/>   
+//         <table style={{ zIndex: "10", position: "absolute", boxShadow: "0 6px 12px rgba(0,0,0,.175)", left: "222px" }}>       
+//           <tbody>
+//             {this.props.linhas.map(linha => 
+//               <ComplexRow linha={linha} selecionouEpi={this.props.selecionouEpi} selecionouEpiBool={this.props.selecionouEpiBool} />)}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+
+
+class SimpleRow extends React.Component {
+  selecionou() {
+    this.props.selecionou(this.props.linha); //passar dados do CA selecionado na pesquisa para componente de cima por props
+    this.props.selecionouBool(true); 
   }
-}
-
-
-class Row extends React.Component {
-  selecionouFuncionario() {
-    this.props.selecionouFuncionario(this.props.linha); //passar dados do CA selecionado na pesquisa para componente de cima por props
-    this.props.selecionouFuncionarioBool(true); 
-  }
+  
   render() {
-    
-    return (
-      <tr style={{border: "1px solid #ced4da", borderRadius: "5px", backgroundColor: "white" }} className="align-items-start d-flex flex-column p-3" onClick={this.selecionouFuncionario.bind(this)}>
+    var linha = [];
 
-        <span>Nome: <b>{this.props.linha.nome}</b></span>
-        <span>RG:  <b>{this.props.linha.rg}</b></span>
-        <span>CPF:  <b>{this.props.linha.cpf}</b></span>
-        <span>CC:  <b>{this.props.linha.cc}</b></span>
+    Object.keys(this.props.linha).map((campo) => 
+      linha.push(<span className="p-2"> {campo}: <b>{this.props.linha[campo]}</b></span>)
+    )   
+
+    return (
+      <tr style={{border: "1px solid #ced4da", borderRadius: "5px", backgroundColor: "white" }} className="align-items-start d-flex flex-column p-3" onClick={this.selecionou.bind(this)}>
+        {linha}
       </tr>
-    );
+    )    
+
   }
 }
 
 
-class Row2 extends React.Component {
+
+
+
+
+
+
+
+
+class ComplexRow extends React.Component {
   state = {
     qntd: ""
   }
